b801e87493e6c66fafefd210fb7f6251a7176b39
J Young <vovrwm@gmail.com>
2022-03-15 07:13:17 +0000

    20220315

--- Lv02/2525.c
+++ Lv02/2525.c
@@ -2,10 +2,32 @@
 
 int main(void)
 {
-	unsigned int H, M, time;
+	unsigned int A, B, C;
 	
-	scanf("%d %d", &H, &M);
-	scanf("%d", &time);
+	do
+	{
+	scanf("%d %d", &A, &B);
+	scanf("%d", &C);
+	}
+	while(A > 23 || B > 59 || C > 1000);
 	
+	B += C;
 	
+	if(B > 59)
+	{
+		int H = B / 60;
+		B -= H*60;
+		
+		A += H;
+	}
+	
+	if(A > 23)
+	{
+		A -= 24;
+	}
+
+	
+	printf("%d %d\n", A, B);
+	
+	return 0;
 }
